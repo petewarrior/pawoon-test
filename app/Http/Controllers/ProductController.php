b3a900a637ac3eb1747b991df06fd25fc6f323ea
Arya Antaputra <petewarrior@gmail.com>
2017-06-19 13:19:23 +0700

    initial commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $actions = '<a href="{{ route("product.edit", ["id" => $id]) }}"><i class="fa fa-edit"></i></a>';
+
+        $grid = \DataGrid::source(new Product);  //same source types of DataSet
+   
+        $grid->add('name','Name', true); //field name, label, sortable
+        $grid->add('price','Price', true); //relation.fieldname
+        //$grid->add($actions, 'Actions', false);
+        $grid->edit('/articles/edit', 'Edit','modify|delete'); //shortcut to link DataEdit actions
+
+        $grid->link('/articles/edit',"Add New", "TR");  //add button
+        $grid->orderBy('id','asc'); //default orderby
+        $grid->paginate(10); //pagination
+
+        return view('product-list', compact('grid'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+        if(!\App::environment('production')) {
+            $this->app->register('Laracasts\Generators\GeneratorsServiceProvider');
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+            
+        }
+    }
+}
